73c05f4fbf30e2c53582a4aa5eee92ffd13e1836
Kyle <KyleMT94@live.com>
2024-10-21 14:33:39 +1300

    Updated logging across the repo
    Added logs into the POM and within utility functions
    Added logging to flag the start and end of scenarios
    Updated the readme to reflect these changes
    Updated the screenshot filename due to scenario name breaking the
    filepath in some situations

--- features/environment.py
+++ features/environment.py
@@ -1,4 +1,5 @@
 from time import time
+import logging
 
 from behave import fixture, use_fixture
 from behave.api.async_step import async_run_until_complete
@@ -36,6 +37,7 @@ async def browser_chrome(context):
 
 @async_run_until_complete
 async def before_scenario(context, scenario):
+    logging.info(f"==== Start scenario {scenario.name} ===")
     await use_fixture(browser_chrome, context)
 
 
@@ -43,13 +45,19 @@ async def before_scenario(context, scenario):
 async def before_feature(context, feature):
     for scenario in feature.scenarios:
         if "repeat5" in scenario.effective_tags:
+            logging.info("Repeating scenario 5 times")
             patch_scenario_to_repeat(scenario, repeats=5)
 
 
 @async_run_until_complete
 async def after_step(context, step):
     if step.status == "failed":
-        screenshot_filename = f"{''.join(step.name.split())}-{int(time())}.png"
+        screenshot_filename = f"{int(time())}.png"
         screenshot_path = f"./output/screenshots/{screenshot_filename}"
+        logging.debug(f"Step failed: {step.name} saving screenshot in {screenshot_filename}")
         await context.page.screenshot(path=screenshot_path, full_page=True)
         image_attachments.attach_image_file(context, screenshot_path)
+
+@async_run_until_complete
+async def after_scenario(context, scenario):
+    logging.info("=== Scenario finished ===")
--- models/contact_page.py
+++ models/contact_page.py
@@ -1,3 +1,5 @@
+import logging 
+
 from playwright.async_api import Page
 
 from models.base_page import BasePage
@@ -18,13 +20,17 @@ class ContactPage(BasePage):
         self.feedback_header = page.get_by_text("Sending Feedback")
 
     async def submit_form(self):
+        logging.info("Pressing submit button")
         await self.submit_button.click()
 
     async def input_forename(self, forename):
+        logging.info(f"Filling forename input field with {forename}")
         await self.forename_text_input.fill(forename)
 
-    async def input_email(self, forename):
-        await self.email_text_input.fill(forename)
+    async def input_email(self, email):
+        logging.info(f"Filling email field with {email}")
+        await self.email_text_input.fill(email)
 
-    async def input_message(self, forename):
-        await self.message_text_input.fill(forename)
+    async def input_message(self, message):
+        logging.info(f"Filling message field with {message}")
+        await self.message_text_input.fill(message)
